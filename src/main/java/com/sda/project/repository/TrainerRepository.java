3397e4964dd87f1ce0ee11424666068887b0db66
cosmin bucur <cosmin.bucur@kambi.com>
2021-11-01 09:11:12 +0200

    code review - fix security

---
+++ src/main/java/com/sda/project/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.sda.project.repository;
+
+import com.sda.project.model.Trainer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainerRepository extends JpaRepository<Trainer, Long> {
+}
